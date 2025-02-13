0de99c5da58cc8dbc9cdc2cb2cd7db0b812f38c3
Devashish Dixit <109742026+Devashish-dixit@users.noreply.github.com>
2025-02-13 19:51:51 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Create a directory for wkhtmltopdf
+
+# Set up directories
 mkdir -p $HOME/wkhtmltopdf
 
 # Download precompiled wkhtmltopdf binary
@@ -8,8 +9,9 @@ wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmlt
 # Extract the .deb package manually
 dpkg-deb -x $HOME/wkhtmltox.deb $HOME/wkhtmltopdf
 
-# Set executable permissions
-chmod +x $HOME/wkhtmltopdf/usr/local/bin/wkhtmltopdf
+# Set the correct path for the binary
+export WKHTMLTOPDF_PATH="$HOME/wkhtmltopdf/usr/local/bin/wkhtmltopdf"
 
 # Add to PATH
-echo "export PATH=\$HOME/wkhtmltopdf/usr/local/bin:\$PATH" >> $HOME/.profile
+echo "export PATH=$HOME/wkhtmltopdf/usr/local/bin:\$PATH" >> $HOME/.profile
+echo "export WKHTMLTOPDF_PATH=$HOME/wkhtmltopdf/usr/local/bin/wkhtmltopdf" >> $HOME/.profile
