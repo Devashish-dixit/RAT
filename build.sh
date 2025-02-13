ba61dfeb803070072b1be666c68d8adb26ec92ed
Devashish Dixit <109742026+Devashish-dixit@users.noreply.github.com>
2025-02-13 19:40:31 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+# Create a directory for wkhtmltopdf
+mkdir -p $HOME/wkhtmltopdf
 
 # Download precompiled wkhtmltopdf binary
-wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.buster_amd64.deb
+wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.buster_amd64.deb -O $HOME/wkhtmltox.deb
 
-# Extract the binary manually
-dpkg-deb -x wkhtmltox_0.12.6-1.buster_amd64.deb $HOME/wkhtmltopdf
+# Extract the .deb package manually
+dpkg-deb -x $HOME/wkhtmltox.deb $HOME/wkhtmltopdf
 
-# Set the environment variable for wkhtmltopdf
+# Set executable permissions
+chmod +x $HOME/wkhtmltopdf/usr/local/bin/wkhtmltopdf
+
+# Add to PATH
 echo "export PATH=\$HOME/wkhtmltopdf/usr/local/bin:\$PATH" >> $HOME/.profile
